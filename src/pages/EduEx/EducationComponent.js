24c30da500a304490dc2cc6f337d97577f3ac4cf
Mayank Panwar <124695013+PANWAR-MAYANK@users.noreply.github.com>
2025-07-11 21:17:56 +0530

    Update EducationComponent.js

--- src/pages/EduEx/EducationComponent.js
+++ src/pages/EduEx/EducationComponent.js
@@ -28,7 +28,7 @@ class Education extends Component {
                   Experience &#38; Education
                 </h1>
                 <h3 className="heading-sub-text" style={{ color: theme.text }}>
-                  Basic Qualification and Certifcations
+                  Basic Qualification and Certifications
                 </h3>
                 <CompetitiveSites logos={competitiveSites.competitiveSites} />
               </div>
